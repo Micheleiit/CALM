707571ebcdf4d671bcab032955f8ed229ff1b8b7
Marco.Cavaletti <marco.cavaletti@iit.it>
2024-10-22 16:40:20 +0200

    firmwares aggiornati + simulazioni

---
+++ pen_firmware_updated/interpolazione Catmull-Rom/code/Catmull_v3.1/trajectory.h
@@ -0,0 +1,66 @@
+/*
+* trajectory.h
+*
+* Created on: September, 2024
+*   Author: Michele Di Lucchio
+*   Description: implementa una Macchina a Stati Finiti (FSM), una struttura logica che permette al sistema di comportarsi in maniera differente a seconda dello stato corrente in cui si trova 
+*/
+
+#ifndef TRAJECTORY_H
+#define TRAJECTORY_H
+
+/************************************************************** Define macros *********************************************************/
+/**************************************************************************************************************************************/
+
+// Limited by the on-board RAM memory (32KB). Could be improved by recording directly the ticks (relative position, 8bits instead of 32)
+// Lasts approx 25 sec
+#define TRAJECTORY_MAX_SIZE 1000 // 2500
+#define STREAMING_PERIOD_ms 1
+#define MIN_EUCLID_DIST 8
+/**************************************************************************************************************************************/
+
+/************************************************************* Include Files **********************************************************/
+/**************************************************************************************************************************************/
+#include <Arduino.h>
+#include "InterpolationLib.h"
+#include "led_blinker.h"
+/**************************************************************************************************************************************/
+
+
+/************************************************************* Type Definitions *******************************************************/
+/**************************************************************************************************************************************/
+typedef struct trajectory_calm
+{
+  // Recorded absolute positions
+  //int32_t pos_x[TRAJECTORY_MAX_SIZE]; // array di 2500 elementi ciascuno per memorizzare le coordinate assolute della traiettoria lungo l'asse x 
+  //int32_t pos_y[TRAJECTORY_MAX_SIZE]; // array di 2500 elementi ciascuno per memorizzare le coordinate assolute della traiettoria lungo l'asse y
+
+  uint16_t current_index_record; // Memorizza l'indice corrente durante la registrazione della traiettoria 
+
+  uint16_t current_size; // Tiene traccia della lunghezza corrente della traiettoria registrata
+  int current_direction; //  indica la direzione attuale della riproduzione della traiettoria
+  uint32_t total_distance;
+
+  double pointX;
+
+  // Creazione dei vettori di double per gli indici e le coordinate x e y
+  double pos_x_double[TRAJECTORY_MAX_SIZE];
+  double pos_y_double[TRAJECTORY_MAX_SIZE];
+  double index_values[TRAJECTORY_MAX_SIZE];
+
+} trajectory_calm;
+/**************************************************************************************************************************************/
+
+/************************************************************* Function Declarations **************************************************/
+/**************************************************************************************************************************************/
+trajectory_calm* init_trajectory_struct();
+boolean record_trajectory(trajectory_calm* traj, int32_t x_to_be_recorded, int32_t y_to_be_recorded, int32_t min_distance);
+void reinit_trajectory(trajectory_calm* traj);
+boolean read_and_interp_trajectory(trajectory_calm* traj, int32_t* x, int32_t* y, double query_points_dist);
+int32_t pow2(int32_t input);
+
+// Dichiarazione del puntatore traj_record come variabile globale accessibile da altri file .cpp
+extern trajectory_calm* traj_record;
+/**************************************************************************************************************************************/
+
+#endif /* TRAJECTORY_H*/
---
+++ pen_firmware_updated/interpolazione lineare/code/linear_interp_v2.1/pen_button.cpp
@@ -0,0 +1,151 @@
+/*
+* pen_button.cpp
+*
+* Created on: August, 2024
+*   Author: Michele Di Lucchio
+*    
+*/
+
+#include "pen_button.h"
+#include "fsm.h"
+#include "trajectory.h"
+
+#include "pin_map.h" // per DEBUG
+
+void initButtonStruct(ButtonStruct* buttonStruct){
+
+  buttonStruct->button = NOT_PRESSED;
+  buttonStruct->PressTime = 0; 
+  buttonStruct->buttonClickCount = 0;
+  
+}
+
+/************************************************************************* FUNZIONI WEAK DI MouseController **************************************************************/
+
+extern USBManager   usbManager;
+extern ButtonStruct rightButton;
+extern ButtonStruct leftButton;
+extern ButtonStruct middleButton;
+
+extern float* pScalingMouse;  // Dichiara che pScalingMouse è definito altrove
+
+
+void mousePressed(){
+
+  if (leftButton.button == NOT_PRESSED){ // Controlla che il pulsante sinistro non sia premuto
+
+    if(usbManager.mouse.getButton(RIGHT_BUTTON) && rightButton.button == NOT_PRESSED){
+      rightButton.button = PRESSED;
+      rightButton.PressTime = millis(); 
+    }
+
+    if(usbManager.mouse.getButton(MIDDLE_BUTTON) && middleButton.button == NOT_PRESSED){
+      middleButton.button = PRESSED;
+      middleButton.PressTime = millis();
+
+    }
+  }  
+  
+  if(rightButton.button == NOT_PRESSED && middleButton.button == NOT_PRESSED){
+    
+    if(usbManager.mouse.getButton(LEFT_BUTTON) && leftButton.button == NOT_PRESSED){
+      leftButton.button = PRESSED;
+      leftButton.PressTime = millis();
+
+    }
+
+  }
+  
+}
+
+void mouseReleased() {
+
+  // Gestione del pulsante destro 
+  if (!usbManager.mouse.getButton(RIGHT_BUTTON) && rightButton.button == PRESSED) {
+
+    unsigned long elapsedTime = millis() - rightButton.PressTime;
+
+    if (elapsedTime <= DEBOUNCE_DELAY) {
+
+      rightButton.button = CLICKED; // Click rapido
+      *pScalingMouse /= 2;
+            
+      // Controlla che SCALING_MOUSE non sia inferiore al limite minimo
+      if (*pScalingMouse < MIN_SCALING_MOUSE) *pScalingMouse = MIN_SCALING_MOUSE;
+
+    } else {
+
+      rightButton.button = LONG_PRESSED; // Pressione lunga
+
+      if(current_state == FREE_HAND){
+        *pScalingMouse = 1.0;               // Reset del fattore di scala
+      }
+
+    }
+
+    rightButton.button = NOT_PRESSED; // Reset stato alla fine
+    
+  }
+
+  // Gestione del pulsante centrale
+  if (!usbManager.mouse.getButton(MIDDLE_BUTTON) && middleButton.button == PRESSED) {
+
+    unsigned long elapsedTime = millis() - middleButton.PressTime;
+
+    if (elapsedTime <= DEBOUNCE_DELAY) {
+      
+      middleButton.button = CLICKED; // Click rapido
+      *pScalingMouse *= 2;
+
+      // Controlla che SCALING_MOUSE non superi il limite massimo
+      if (*pScalingMouse > MAX_SCALING_MOUSE) *pScalingMouse = MAX_SCALING_MOUSE;
+
+    } else {
+
+      middleButton.button = LONG_PRESSED; // Pressione lunga
+
+      if(current_state == FREE_HAND){
+        *pScalingMouse = 1.0;               // Reset del fattore di scala
+      }
+      
+    }
+
+    middleButton.button = NOT_PRESSED; // Reset stato alla fine
+
+  }
+
+  // Gestione del pulsante sinistro
+  if (!usbManager.mouse.getButton(LEFT_BUTTON) && leftButton.button == PRESSED) {
+
+    unsigned long elapsedTime = millis() - leftButton.PressTime;
+
+    if (elapsedTime <= DEBOUNCE_DELAY) { // Click rapido per ZOOM_IN
+      
+      leftButton.button = CLICKED; // Click rapido
+
+      if(current_state == DRAW_RECORD){
+
+        reinit_trajectory(traj_record);
+        current_state = FREE_HAND;
+        
+      }
+
+    } else {
+
+      leftButton.button = LONG_PRESSED; // Pressione lunga
+      
+      if(current_state == RECORDING) {
+        
+        //traj_record->current_index = traj_record->current_index_record; //Synchronize the reading with the last writing: serve a garantire che la riproduzione della traiettoria inizi dall'ultimo punto registrato
+        traj_record->current_index = traj_record->current_size - 1;
+        current_state = DRAW_RECORD;
+
+      }
+
+    }
+
+    leftButton.button = NOT_PRESSED; // Reset stato alla fine
+    
+  }
+
+}
